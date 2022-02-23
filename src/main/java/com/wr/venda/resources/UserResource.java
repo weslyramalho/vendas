b37afde3eeb820adf27a428b5fc483f3e912824c
Wesly <weslyramalho@yahoo.com.br>
2022-02-23 18:34:40 -0300

    Outros

--- src/main/java/com/wr/venda/resources/UserResource.java
+++ src/main/java/com/wr/venda/resources/UserResource.java
@@ -1,5 +1,20 @@
 package com.wr.venda.resources;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wr.venda.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
 public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456"); 
+		return ResponseEntity.ok().body(u1);
+	}
 
 }
