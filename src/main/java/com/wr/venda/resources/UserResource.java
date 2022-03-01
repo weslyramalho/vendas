ed4028e05e200abaab7fc81c2c1e0bccc250658c
Wesly <weslyramalho@yahoo.com.br>
2022-03-01 17:01:26 -0300

    UserService

--- src/main/java/com/wr/venda/resources/UserResource.java
+++ src/main/java/com/wr/venda/resources/UserResource.java
@@ -1,20 +1,33 @@
 package com.wr.venda.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wr.venda.entities.User;
+import com.wr.venda.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456"); 
-		return ResponseEntity.ok().body(u1);
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
