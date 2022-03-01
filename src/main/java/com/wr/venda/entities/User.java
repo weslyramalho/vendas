1e6b1d4d8e596178f641a44722f9630d7cce4da2
Wesly <weslyramalho@yahoo.com.br>
2022-03-01 18:00:03 -0300

    order

--- src/main/java/com/wr/venda/entities/User.java
+++ src/main/java/com/wr/venda/entities/User.java
@@ -1,14 +1,19 @@
 package com.wr.venda.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "tb_user")
 public class User implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
@@ -20,6 +25,9 @@ public class User implements Serializable{
 	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {
 	
@@ -88,6 +96,10 @@ public class User implements Serializable{
 			return false;
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
+	}
+
+	public List<Order> getOrders() {
+		return orders;
 	}
 	
 	
