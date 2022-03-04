887c037503dc4f33ce7e54aabf6098bf71d86b58
Wesly <weslyramalho@yahoo.com.br>
2022-03-04 15:59:53 -0300

    User Update

--- src/main/java/com/wr/venda/services/UserService.java
+++ src/main/java/com/wr/venda/services/UserService.java
@@ -3,6 +3,8 @@ package com.wr.venda.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.Entity;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -30,6 +32,19 @@ public class UserService {
 	
 	public void delete(Long id) {
 		repository.deleteById(id);
+	}
+	
+	public User update(Long id, User obj) {
+		User entity = repository.getOne(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+		
 	}
 
 }
