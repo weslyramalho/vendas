5d01da2e675a8fff827045ad5ab66e8adce7ba57
Wesly <weslyramalho@yahoo.com.br>
2022-03-04 14:56:34 -0300

    Payment

--- src/main/java/com/wr/venda/config/TestConfig.java
+++ src/main/java/com/wr/venda/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.wr.venda.entities.Category;
 import com.wr.venda.entities.Order;
 import com.wr.venda.entities.OrderItem;
+import com.wr.venda.entities.Payment;
 import com.wr.venda.entities.Product;
 import com.wr.venda.entities.User;
 import com.wr.venda.entities.enums.OrderStatus;
@@ -80,6 +81,11 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 
 }
--- src/main/java/com/wr/venda/entities/OrderItem.java
+++ src/main/java/com/wr/venda/entities/OrderItem.java
@@ -40,7 +40,6 @@ public class OrderItem implements Serializable{
 		id.setOrder(order);
 	}
 	
-	@JsonIgnore
 	public Product getProduct() {
 		return id.getProduct();
 		}
